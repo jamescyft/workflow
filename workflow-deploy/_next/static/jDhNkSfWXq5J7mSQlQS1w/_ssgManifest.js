cb45e9fc9a46b802858c5f849e87b28c81e1049d
jamescyft <james@cyft.ai>
2025-04-06 22:34:39 -0400

    Add files via upload

---
+++ workflow-deploy/_next/static/jDhNkSfWXq5J7mSQlQS1w/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fai-approaches","\u002Fdesign-specifications","\u002Fimplementation-plan","\u002Fprototype-analysis","\u002Fstrategy","\u002Fwow-experience"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
